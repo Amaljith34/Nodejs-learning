307fcad7fef8dccd4ac2790b44009785b88d882c
Amaljith-aj <jith51541@gmail.com>
2024-08-31 15:31:59 +0530

    add inpu/ output queue

--- queues/ioqueue/ioqueue.js
+++ queues/ioqueue/ioqueue.js
@@ -1,20 +1,20 @@
 const fs=require('fs');
-// const { setTimeout } = require('timers/promises');
-// fs.writeFile('sample.txt','hello world',(err)=>{
-//     if(err){
-//         console.log(err);
+const { setTimeout } = require('timers/promises');
+fs.writeFile('sample.txt','hello world',(err)=>{
+    if(err){
+        console.log(err);
         
-//     }
-//     else{
-//         console.log('sucess');
-//     }
+    }
+    else{
+        console.log('sucess');
+    }
     
-// })
+})
 setTimeout(()=>console.log('this isn set time out'),0);
 fs.readFile('__faname',()=>{
     console.log('this is readfile');
-    // console.log(data);
+    console.log(data);
 })
-//  process.nextTick(()=>console.log('heloo'))
-//  Promise.resolve().then(()=>console.log('this is promise .resolve')
-//  )
+ process.nextTick(()=>console.log('heloo'))
+ Promise.resolve().then(()=>console.log('this is promise .resolve')
+ )
