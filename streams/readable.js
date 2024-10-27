de5d853a3e344511916aba04990488cb15e6a080
AMALJITH K <156279690+Amaljith34@users.noreply.github.com>
2024-10-27 22:41:30 +0530

    Update readable.js

--- streams/readable.js
+++ streams/readable.js
@@ -1,13 +1,13 @@
-// const fs=require('fs');
-// const readable=fs.createReadStream('./test.txt');
-// readable.on('data',(chunk)=>{
-//     console.log(`recive ${chunk.length} byteof data`);
+const fs=require('fs');
+const readable=fs.createReadStream('./test.txt');
+readable.on('data',(chunk)=>{
+    console.log(`recive ${chunk.length} byteof data`);
     
-// })
-// readable.on('end',()=>{
-//     console.log('there is no more data');
+})
+readable.on('end',()=>{
+    console.log('there is no more data');
     
-// })
+})
 
 const fs=require('fs')
 const readable=fs.createReadStream('./test.txt');
